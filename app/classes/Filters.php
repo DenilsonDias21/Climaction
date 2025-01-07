83c32a21651c486f62c6fe9aca5b54e4a1d76785
DenilsonDias21 <denilsondias59@gmail.com>
2025-01-07 20:05:26 -0300

    create class Filters for use variables globals in view with twig

---
+++ app/classes/Filters.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace app\classes;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+
+class Filters extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+
+        ];
+    }
+}
