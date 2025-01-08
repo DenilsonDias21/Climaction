42aeceaf92646752f71a8e8386691283e86ac0a9
DenilsonDias21 <denilsondias59@gmail.com>
2025-01-08 17:11:14 -0300

    changing composer line 9 from twig/twig to slim/twig-view latest version * and update composer

--- app/controllers/Site.php
+++ app/controllers/Site.php
@@ -9,6 +9,7 @@ class Site extends Base {
 
     public function index($request, $response)
     {
+
         return $this->getTwig()->render($response, $this->setView('home'), [
             'title'     => 'Home'
         ]);
