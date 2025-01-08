0691f7be9a34f4c0902f7446519f3cd21fb48511
DenilsonDias21 <denilsondias59@gmail.com>
2025-01-08 17:00:57 -0300

    call class base for load file view in home

--- app/controllers/Site.php
+++ app/controllers/Site.php
@@ -2,11 +2,16 @@
 
 namespace app\controllers;
 
-class Site {
+use app\controllers\Base;
 
-    public function index()
+
+class Site extends Base {
+
+    public function index($request, $response)
     {
-        echo 'home';
+        return $this->getTwig()->render($response, $this->setView('home'), [
+            'title'     => 'Home'
+        ]);
     }
 
 }
